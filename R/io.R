b3b645d654b7ce47af3cb662fa605b2c8f6cd7fc
ryanzomorrodi <ryanzomorrodi@gmail.com>
2024-11-17 13:10:02 -0600

    Remove sas_read_* and sas_write_* for now

--- R/io.R
+++ R/io.R
@@ -111,24 +111,3 @@ sas_list <- function(path) {
 
   .pkgenv$session$dirlist(path)
 }
-
-#' @export
-sas_read_csv <- function(file, table_name, libref = "WORK") {
-  check_connection()
-
-  .pkgenv$session$read_csv(file, table_name, libref)
-}
-
-#' @export
-sas_write_csv <- function(table_name, path, libref = "WORK") {
-  check_connection()
-
-  .pkgenv$session$write_csv(path, table_name, libref)
-}
-
-#' @export
-sas_write_parquet <- function(table_name, path, libref = "WORK") {
-  check_connection()
-  
-  .pkgenv$session$sasdata2parquet(path, table_name, libref)
-}
