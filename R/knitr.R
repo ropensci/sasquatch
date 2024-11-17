ceb11e6c72b88a3b63526aa1d9c80fa90018ed11
ryanzomorrodi <ryanzomorrodi@gmail.com>
2024-11-16 19:24:32 -0600

    Export SAS engine and add docs

--- R/knitr.R
+++ R/knitr.R
@@ -1,4 +1,26 @@
-sashtml_engine <- function (options) {
+#' A SAS HTML5 engine for `knitr`
+#' 
+#' @param options Options from `knitr`.
+#' 
+#' @details
+#' Will be activated by running `library(sasquatch)`
+#' 
+#' ### Supported `knitr` chunk options
+#' `sasquatch`'s engine implements may of the same options as the R engine in
+#' `knitr`, but not all.
+#' 
+#' - `eval` (Default: `TRUE`): Evaluate the code chunk (if false, just echos the code into the output)
+#' - `echo` (Default: `TRUE`): Include the source code in output
+#' - `output` (Default: `TRUE`): Include the results of executing the code in 
+#' the output (`TRUE` or `FALSE`).
+#' - `include` (Default: `TRUE`): Include any output (code or results).  
+#' - `capture` (Default: `"both"`): If `"both"`, tabpanel with output and log included. 
+#' If `"lst"`, only output is included. If `"log"` only log is included.
+#' 
+#' @return `knitr` engine output.
+#' 
+#' @export
+sas_engine <- function (options) {
   code <- paste(options$code, collapse = "\n")
 
   if (!isTRUE(getOption('knitr.in.progress'))) {
@@ -7,7 +29,7 @@ sashtml_engine <- function (options) {
 
   if (identical(options$include, FALSE)) {
     options$echo <- FALSE
-    options$outputv <- FALSE
+    options$output <- FALSE
   }
 
   if (identical(options$eval, FALSE)) {
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .pkgenv <- new.env(parent = emptyenv())
 
 .onAttach <- function(libname, pkgname) {
-  knitr::knit_engines$set(sas = sashtml_engine)
+  knitr::knit_engines$set(sas = sas_engine)
 }
