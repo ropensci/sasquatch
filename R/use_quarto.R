df9388cec3c0c389e00e1d1d04e2d4cceaea4d4a
ryanzomorrodi <ryanzomorrodi@gmail.com>
2024-11-16 19:28:01 -0600

    Allow quarto directory specification

--- R/use_quarto.R
+++ R/use_quarto.R
@@ -1,17 +1,27 @@
-#' Add SAS quarto template to current directory
+#' Add SAS quarto template to specified directory
 #' 
 #' @description
-#' Adds SAS quarto template and syntax highlighting XML to current directory.
-#' 
+#' Adds SAS quarto template and syntax highlighting XML to a specified
+#' directory.
+#'
+#' @param path (Default: Current directory) Path to copy quarto template and
+#' syntax highlighting XML into.
+#'  
 #' @return No return value.
 #' 
 #' @export
-sas_use_quarto <- function() {
+sas_use_quarto <- function(path) {
+  if (!hasArg(path)) {
+    path <- "."
+  } else if (!dir.exists(path)) {
+    stop("Specified directory does not exist")
+  }
+
   template <- system.file("sasquatch.qmd", package = "sasquatch")
-  file.copy(template, ".")
+  file.copy(template, path)
 
   sas_xml <- system.file("sas.xml", package = "sasquatch")
-  file.copy(sas_xml, ".")
+  file.copy(sas_xml, path)
 
   invisible()
 }
