18246ffb6cc6196df73cfb734c1b136c510f6623
ryanzomorrodi <ryanzomorrodi@gmail.com>
2025-08-03 14:18:29 -0500

    specify type in vapply within sas_to_r

--- R/to-r.R
+++ R/to-r.R
@@ -43,7 +43,11 @@ sas_to_r <- function(table_name, libref = "WORK") {
     col[is.nan(col)] <- NA
     col
   }))
-  date_cols <- vapply(x, \(col) inherits(col, "POSIXct"))
+  date_cols <- vapply(
+    x,
+    \(col) inherits(col, "POSIXct"),
+    FUN.VALUE = logical(1)
+  )
   x[date_cols] <- lapply(x[date_cols], function(col) {
     as.POSIXct(format(col, tz = "UTC"))
   })
